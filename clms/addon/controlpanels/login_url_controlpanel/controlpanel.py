90062cb148490bcbf2f6b759cb1b63f64683d2f8
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-10-14 16:08:31 +0200

    pep

--- clms/addon/controlpanels/login_url_controlpanel/controlpanel.py
+++ clms/addon/controlpanels/login_url_controlpanel/controlpanel.py
@@ -10,10 +10,12 @@ from zope import schema
 class ILoginURLControlPanel(Interface):
     login_url = schema.TextLine(
         title=_(
-            u"Enter the URL, relative to the portal root to be used as a login url",
+            "Enter the URL, relative to the portal root to be used "
+            "as a login url",
         ),
         description=_(
-            u"This will be used to login using EU Login and needs to point to the OIDC PAS Plugin",
+            "This will be used to login using EU Login and needs to "
+            "point to the OIDC PAS Plugin",
         ),
         default=u"/api/acl_users/keycloak/login",
         required=True,
