603bce6e36b599df43758bd0f0859f66f754c4ca
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-10-14 16:05:12 +0200

    override the callback view to redirect to the home of the Volto site

---
+++ clms/addon/browser/loginview.py
@@ -0,0 +1,14 @@
+"""
+Override OIDC PAS Plugin redirect url
+"""
+from pas.plugins.oidc.browser.view import CallbackView as BaseCallbackView
+
+
+class CallbackView(BaseCallbackView):
+    """ Callback view """
+    def __call__(self):
+        """
+        Override redirect url
+        """
+        _ = super(CallbackView, self).__call__()
+        return self.request.response.redirect("/", status=302)
--- clms/addon/controlpanels/login_url_controlpanel/controlpanel.py
+++ clms/addon/controlpanels/login_url_controlpanel/controlpanel.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
+"""
+This is the control panel for the login_url
+"""
 from clms.addon import _
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.z3cform import layout
-from zope.interface import Interface
 from zope import schema
+from zope.interface import Interface
 
 
 class ILoginURLControlPanel(Interface):
+    """ Control Panel Schema """
+
     login_url = schema.TextLine(
         title=_(
-            u"Enter the URL, relative to the portal root to be used as a login url",
+            "Enter the URL, relative to the portal root to be used "
+            "as a login url",
         ),
         description=_(
-            u"This will be used to login using EU Login and needs to point to the OIDC PAS Plugin",
+            "This will be used to login using EU Login and needs to "
+            "point to the OIDC PAS Plugin",
         ),
         default=u"/api/acl_users/keycloak/login",
         required=True,
@@ -22,6 +29,8 @@ class ILoginURLControlPanel(Interface):
 
 
 class LoginURLControlPanel(RegistryEditForm):
+    """ control panel rest API endpoint configuration """
+
     schema = ILoginURLControlPanel
     schema_prefix = "clms.addon.login_url_controlpanel"
     label = _("Login URL Control Panel")
