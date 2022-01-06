596accf1098f29d101b936d9419db4ec51ffe946
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-06 10:07:27 +0100

    notification subscriptions

---
+++ clms/addon/utilities/event_notifications_utility.py
@@ -0,0 +1,69 @@
+"""
+A utility to save all subscribers for event notifications
+"""
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+from zope.interface import implementer
+from plone import api
+from zope.annotation.interfaces import IAnnotations
+from persistent.mapping import PersistentMapping
+
+
+ANNOTATION_KEY = "clms.addon.event_notification_subscribers"
+
+
+class IEventNotificationsUtility(Interface):
+    """ Utility interface """
+
+    def subscribe_address(email):
+        """subscribe email address to notifications. Return True if
+        subscription was OK False if the user is already
+        subscribed
+        """
+
+    def unsubscribe_addreess(email):
+        """unsubscribe email address to notifications. Return True if
+        unsubscription was OK False if the user is not subscribed
+        """
+
+    def list_subscribers():
+        """ return list of all subscribers """
+
+
+@implementer(IEventNotificationsUtility)
+class EventNotificationsUtility:
+    """ utility implementation """
+
+    def subscribe_address(self, email):
+        """ subscribe email address"""
+        portal = api.portal.get()
+        annotations = IAnnotations(portal)
+        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
+        if email is not None and email.strip():
+            if email not in subscribers:
+                subscribers[email] = 1
+                annotations[ANNOTATION_KEY] = subscribers
+                return True
+
+        return False
+
+    def unsubscribe_address(self, email):
+        """ unsubscribe email address"""
+        portal = api.portal.get()
+        annotations = IAnnotations(portal)
+        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
+        if email is not None and email.strip():
+            if email in subscribers:
+                del subscribers[email]
+                annotations[ANNOTATION_KEY] = subscribers
+                return True
+
+        return False
+
+    def list_subscribers(self):
+        """ return all subscribers """
+        portal = api.portal.get()
+        annotations = IAnnotations(portal)
+        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
+        return subscribers.keys()
