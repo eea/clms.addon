60f151bbd1542844322d4f7143c8f48d9cf854e6
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-06 15:59:58 +0100

    use a base implementation to avoid code duplications

--- clms/addon/utilities/event_notifications_utility.py
+++ clms/addon/utilities/event_notifications_utility.py
@@ -3,67 +3,34 @@ A utility to save all subscribers for event notifications
 """
 # -*- coding: utf-8 -*-
 
-from zope.interface import Interface
 from zope.interface import implementer
-from plone import api
-from zope.annotation.interfaces import IAnnotations
-from persistent.mapping import PersistentMapping
-from datetime import datetime
+from .base_notifications_utility import (
+    INotificationsUtility,
+    NotificationsUtility,
+)
+from .base_pending_subscriptions_utility import (
+    IPendingSubscriptionHandler,
+    PendingSubscriptionHandler,
+)
 
-ANNOTATION_KEY = "clms.addon.event_notification_subscribers"
 
-
-class IEventNotificationsUtility(Interface):
-    """ Utility interface """
-
-    def subscribe_address(email):
-        """subscribe email address to notifications. Return True if
-        subscription was OK False if the user is already
-        subscribed
-        """
-
-    def unsubscribe_address(email):
-        """unsubscribe email address to notifications. Return True if
-        unsubscription was OK False if the user is not subscribed
-        """
-
-    def list_subscribers():
-        """ return list of all subscribers """
+class IEventNotificationsUtility(INotificationsUtility):
+    pass
 
 
 @implementer(IEventNotificationsUtility)
-class EventNotificationsUtility:
+class EventNotificationsUtility(NotificationsUtility):
     """ utility implementation """
 
-    def subscribe_address(self, email):
-        """ subscribe email address"""
-        portal = api.portal.get()
-        annotations = IAnnotations(portal)
-        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
-        if email is not None and email.strip():
-            if email not in subscribers:
-                subscribers[email] = {"date": datetime.utcnow().isoformat()}
-                annotations[ANNOTATION_KEY] = subscribers
-                return True
+    ANNOTATION_KEY = "clms.addon.event_notification_subscribers"
 
-        return False
 
-    def unsubscribe_address(self, email):
-        """ unsubscribe email address"""
-        portal = api.portal.get()
-        annotations = IAnnotations(portal)
-        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
-        if email is not None and email.strip():
-            if email in subscribers:
-                del subscribers[email]
-                annotations[ANNOTATION_KEY] = subscribers
-                return True
+class IEventPendingSubscriptionsUtility(IPendingSubscriptionHandler):
+    pass
 
-        return False
 
-    def list_subscribers(self):
-        """ return all subscribers """
-        portal = api.portal.get()
-        annotations = IAnnotations(portal)
-        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
-        return [key for key in subscribers.keys()]
+@implementer(IEventPendingSubscriptionsUtility)
+class EventPendingSubscriptionsUtility(PendingSubscriptionHandler):
+    """ utility implementation """
+
+    ANNOTATION_KEY = "clms.addon.event_pending_subscriptions"
--- clms/addon/utilities/newsitem_notifications_utility.py
+++ clms/addon/utilities/newsitem_notifications_utility.py
@@ -1,70 +1,36 @@
 """
-A utility to save all subscribers for newsitem notifications
+A utility to save all subscribers for NewsItem notifications
 """
 # -*- coding: utf-8 -*-
 
-from zope.interface import Interface
 from zope.interface import implementer
-from plone import api
-from zope.annotation.interfaces import IAnnotations
-from persistent.mapping import PersistentMapping
-from datetime import datetime
+from .base_notifications_utility import (
+    INotificationsUtility,
+    NotificationsUtility,
+)
+from .base_pending_subscriptions_utility import (
+    IPendingSubscriptionHandler,
+    PendingSubscriptionHandler,
+)
 
 
-ANNOTATION_KEY = "clms.addon.newsitem_notification_subscribers"
-
-
-class INewsItemNotificationsUtility(Interface):
-    """ Utility interface """
-
-    def subscribe_address(email):
-        """subscribe email address to notifications. Return True if
-        subscription was OK False if the user is already
-        subscribed
-        """
-
-    def unsubscribe_address(email):
-        """unsubscribe email address to notifications. Return True if
-        unsubscription was OK False if the user is not subscribed
-        """
-
-    def list_subscribers():
-        """ return list of all subscribers """
+class INewsItemNotificationsUtility(INotificationsUtility):
+    pass
 
 
 @implementer(INewsItemNotificationsUtility)
-class NewsItemNotificationsUtility:
+class NewsItemNotificationsUtility(NotificationsUtility):
     """ utility implementation """
 
-    def subscribe_address(self, email):
-        """ subscribe email address"""
-        portal = api.portal.get()
-        annotations = IAnnotations(portal)
-        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
-        if email is not None and email.strip():
-            if email not in subscribers:
-                subscribers[email] = {"date": datetime.utcnow().isoformat()}
-                annotations[ANNOTATION_KEY] = subscribers
-                return True
+    ANNOTATION_KEY = "clms.addon.newsitem_notification_subscribers"
 
-        return False
 
-    def unsubscribe_address(self, email):
-        """ unsubscribe email address"""
-        portal = api.portal.get()
-        annotations = IAnnotations(portal)
-        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
-        if email is not None and email.strip():
-            if email in subscribers:
-                del subscribers[email]
-                annotations[ANNOTATION_KEY] = subscribers
-                return True
+class INewsItemPendingSubscriptionsUtility(IPendingSubscriptionHandler):
+    pass
 
-        return False
 
-    def list_subscribers(self):
-        """ return all subscribers """
-        portal = api.portal.get()
-        annotations = IAnnotations(portal)
-        subscribers = annotations.get(ANNOTATION_KEY, PersistentMapping())
-        return [key for key in subscribers.keys()]
+@implementer(INewsItemPendingSubscriptionsUtility)
+class NewsItemPendingSubscriptionsUtility(PendingSubscriptionHandler):
+    """ utility implementation """
+
+    ANNOTATION_KEY = "clms.addon.newsitem_pending_subscriptions"
