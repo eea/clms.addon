88b113c60d7c91bfd5bbe5e6855005a5509e6c93
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-11 15:34:30 +0100

    override eea.restapis blocks validator until a new version is relased

---
+++ clms/addon/handlers.py
@@ -0,0 +1,44 @@
+""" handlers module """
+
+# from plone.app.linkintegrity.utils import referencedRelationship
+import json
+import logging
+from Acquisition import aq_base
+from plone.api.exc import CannotGetPortalError
+from zope.component import queryAdapter
+from eea.restapi.interfaces import IBlockValidator
+
+
+logger = logging.getLogger("clms.addon")
+
+
+_marker = object()
+
+
+def validate_blocks(obj, event):
+    """ validate blocks """
+    base = aq_base(obj)
+    blocks = getattr(base, "blocks", _marker)
+
+    if blocks in (_marker, None):
+        return
+
+    res = {}
+
+    try:
+        # blocks is *sometimes* a json encoded string
+        blocks = json.loads(blocks)
+    except Exception:
+        pass
+    for k, v in blocks.items():
+        validator = queryAdapter(obj, IBlockValidator, name=v.get("@type", ""))
+
+        if validator is not None:
+            try:
+                res[k] = validator.clean(v)
+            except CannotGetPortalError:
+                res[k] = v
+        else:
+            res[k] = v
+
+    obj.blocks = res
