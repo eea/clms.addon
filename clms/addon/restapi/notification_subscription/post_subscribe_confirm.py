259e99e5f0ccf3bc86ceeaa47fc2d7c1650580f5
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-19 10:52:03 +0100

    return proper no-content results

--- clms/addon/restapi/notification_subscription/post_subscribe_confirm.py
+++ clms/addon/restapi/notification_subscription/post_subscribe_confirm.py
@@ -4,17 +4,20 @@ REST API information for notification subscriptions
 # -*- coding: utf-8 -*-
 
 from plone.protect.interfaces import IDisableCSRFProtection
-from plone.restapi.services import Service
+from plone.restapi.services import Service, _no_content_marker
 from zope.component import getUtility
 from zope.interface import alsoProvides, implementer
 from zope.publisher.interfaces import IPublishTraverse
 
-from clms.addon.utilities.event_notifications_utility import \
-    IEventPendingSubscriptionsUtility
-from clms.addon.utilities.newsitem_notifications_utility import \
-    INewsItemPendingSubscriptionsUtility
-from clms.addon.utilities.newsletter_utility import \
-    INewsLetterPendingSubscriptionsUtility
+from clms.addon.utilities.event_notifications_utility import (
+    IEventPendingSubscriptionsUtility,
+)
+from clms.addon.utilities.newsitem_notifications_utility import (
+    INewsItemPendingSubscriptionsUtility,
+)
+from clms.addon.utilities.newsletter_utility import (
+    INewsLetterPendingSubscriptionsUtility,
+)
 
 
 @implementer(IPublishTraverse)
@@ -55,7 +58,7 @@ class BaseNotificationsSubscribeConfirmHandler(Service):
             utility = getUtility(self.utility_interface)
             if utility.confirm_pending_subscription(self._get_key):
                 self.request.response.setStatus(204)
-                return
+                return _no_content_marker
 
             self.request.response.setStatus(400)
             return {"error": "Provided key is not valid"}
