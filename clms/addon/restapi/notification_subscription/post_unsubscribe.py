01c0aad3f1d9c476dbe4aefa915a320f1089cca4
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-24 10:47:30 +0100

    check if the user is subscribed before sending the confirmation email

--- clms/addon/restapi/notification_subscription/post_unsubscribe.py
+++ clms/addon/restapi/notification_subscription/post_unsubscribe.py
@@ -19,13 +19,13 @@ from zope.interface import alsoProvides
 
 from clms.addon import _
 from clms.addon.utilities.event_notifications_utility import (
-    IEventPendingUnSubscriptionsUtility,
+    IEventPendingUnSubscriptionsUtility, IEventNotificationsUtility
 )
 from clms.addon.utilities.newsitem_notifications_utility import (
-    INewsItemPendingUnSubscriptionsUtility,
+    INewsItemPendingUnSubscriptionsUtility, INewsItemNotificationsUtility
 )
 from clms.addon.utilities.newsletter_utility import (
-    INewsLetterPendingUnSubscriptionsUtility,
+    INewsLetterPendingUnSubscriptionsUtility, INewsLetterNotificationsUtility
 )
 
 
@@ -38,6 +38,18 @@ class BaseNotificationsUnSubscribeHandler(Service):
         raise NotImplementedError(
             "You need to define the interface in your class"
         )
+
+
+    @property
+    def subscription_handler_utility(self):
+        """utility that really handles the subscription. Will be used
+        to check if the said e-mail address is already subscribed
+        and if so inform the user with a message
+        """
+        raise NotImplementedError(
+            "You need to define the interface in your class"
+        )
+
 
     @property
     def registry_key_for_base_url(self):
@@ -57,26 +69,40 @@ class BaseNotificationsUnSubscribeHandler(Service):
             "You need to define the message in your class"
         )
 
+    def email_message_not_subscribed(self, url, portal_tilte):
+        """return the email message to be sent to the user when she is
+        not subscribed
+        """
+        raise NotImplementedError(
+            "You need to define the message in your class"
+        )
+
     def reply(self):
         """ return the real response """
         alsoProvides(self.request, IDisableCSRFProtection)
         body = json_body(self.request)
         email = body.get("email")
         if email is not None:
-            utility = getUtility(self.utility_interface)
-            key = utility.create_pending_unsubscription(email)
-            status = self.send_confirmation_email(email, key)
-            if status:
-                self.request.response.setStatus(204)
-                return _no_content_marker
+            subscription_utility = getUtility(self.subscription_handler_utility)
+            if subscription_utility.is_subscribed(email):
+                utility = getUtility(self.utility_interface)
+                key = utility.create_pending_unsubscription(email)
+                status = self.send_confirmation_email(email, key)
+                if status:
+                    self.request.response.setStatus(204)
+                    return _no_content_marker
 
-            self.request.response.setStatus(500)
-            return {
-                "status": "error",
-                "message": (
-                    "There was an error sending the email, try again please"
-                ),
-            }
+                self.request.response.setStatus(500)
+                return {
+                    "status": "error",
+                    "message": (
+                        "There was an error sending the email, try again please"
+                    ),
+                }
+
+            self.send_not_subscribed_email(email)
+            self.request.response.setStatus(204)
+            return _no_content_marker
 
         self.request.response.setStatus(400)
         return {"status": "error", "message": "No email address provided"}
@@ -131,11 +157,60 @@ class BaseNotificationsUnSubscribeHandler(Service):
 
         return True
 
+    def send_not_subscribed_email(self, email):
+        """send an email to the user stating that she is not subsribed"""
+        registry = getUtility(IRegistry)
+        url = registry.get(self.registry_key_for_base_url)
+
+        frontend_domain = api.portal.get_registry_record(
+            "volto.frontend_domain"
+        )
+        if frontend_domain.endswith("/"):
+            frontend_domain = frontend_domain[:-1]
+
+        url = frontend_domain + "/en" + url
+
+        registry = getUtility(IRegistry)
+        mail_settings = registry.forInterface(IMailSchema, prefix="plone")
+        from_address = mail_settings.email_from_address
+        encoding = registry.get("plone.email_charset", "utf-8")
+        host = api.portal.get_tool("MailHost")
+        registry = getUtility(IRegistry)
+        site_settings = registry.forInterface(
+            ISiteSchema, prefix="plone", check=False
+        )
+        portal_title = site_settings.site_title
+        subject = self.email_subject
+        message = self.email_message_not_subscribed(url, portal_title)
+
+        message = MIMEText(message, "plain", encoding)
+        message["Reply-To"] = from_address
+        try:
+            host.send(
+                message.as_string(),
+                email,
+                from_address,
+                subject=subject,
+                charset=encoding,
+            )
+
+        except (SMTPException, RuntimeError):
+            plone_utils = api.portal.get_tool("plone_utils")
+            exception = plone_utils.exceptionString()
+            message = "Unable to send mail: {exception}".format(
+                exception=exception
+            )
+
+            return False
+
+        return True
+
 
 class NewsItemNotificationsUnSubscribe(BaseNotificationsUnSubscribeHandler):
     """News Item implementation """
 
     utility_interface = INewsItemPendingUnSubscriptionsUtility
+    subscription_handler_utility = INewsItemNotificationsUtility
     # pylint: disable=line-too-long
     registry_key_for_base_url = "clms.addon.notifications_controlpanel.newsitem_notification_unsubscriptions_url"  # noqa
     email_subject = _("News item notification unsubscription")
@@ -155,11 +230,30 @@ class NewsItemNotificationsUnSubscribe(BaseNotificationsUnSubscribeHandler):
             )
         )
 
+    def email_message_not_subscribed(self, url, portal_title):
+        """ return the message """
+        return translate(
+            _(
+                "You are receiving this email because you have requested to"
+                " unsubscribe from receiving notifications about news items"
+                " from the ${portal_title} website at ${url}. We are glad to "
+                " inform you that you are not subscribed to receive those "
+                " notifications. This requires no additional action on your "
+                " part.",
+                mapping={
+                    "portal_title": portal_title,
+                    "url": url,
+                },
+            )
+        )
+
+
 
 class EventNotificationsUnSubscribe(BaseNotificationsUnSubscribeHandler):
     """ base class """
 
     utility_interface = IEventPendingUnSubscriptionsUtility
+    subscription_handler_utility = IEventNotificationsUtility
     # pylint: disable=line-too-long
     registry_key_for_base_url = "clms.addon.notifications_controlpanel.event_notification_unsubscriptions_url"  # noqa
     email_subject = _("Event notification unsubscription")
@@ -179,11 +273,30 @@ class EventNotificationsUnSubscribe(BaseNotificationsUnSubscribeHandler):
             )
         )
 
+    def email_message_not_subscribed(self, url, portal_title):
+        """ return the message """
+        return translate(
+            _(
+                "You are receiving this email because you have requested to"
+                " unsubscribe from receiving notifications about events"
+                " from the ${portal_title} website at ${url}. We are glad to "
+                " inform you that you are not subscribed to receive those "
+                " notifications. This requires no additional action on your "
+                " part.",
+                mapping={
+                    "portal_title": portal_title,
+                    "url": url,
+                },
+            )
+        )
+
+
 
 class NewsLetterNotificationsUnSubscribe(BaseNotificationsUnSubscribeHandler):
     """ base class """
 
     utility_interface = INewsLetterPendingUnSubscriptionsUtility
+    subscription_handler_utility = INewsLetterNotificationsUtility
     # pylint: disable=line-too-long
     registry_key_for_base_url = "clms.addon.notifications_controlpanel.newsletter_notification_unsubscriptions_url"  # noqa
     email_subject = _("NewsLetter notification unsubscription")
@@ -193,9 +306,26 @@ class NewsLetterNotificationsUnSubscribe(BaseNotificationsUnSubscribeHandler):
         return translate(
             _(
                 "You are receiving this email because you have requested to"
-                " unsubscribe from receivingthe newsletter from"
+                " unsubscribe from receiving the newsletter from"
                 " the ${portal_title} website. Please visit the following URL"
                 " to confirm your subscription: ${url}.",
+                mapping={
+                    "portal_title": portal_title,
+                    "url": url,
+                },
+            )
+        )
+
+    def email_message_not_subscribed(self, url, portal_title):
+        """ return the message """
+        return translate(
+            _(
+                "You are receiving this email because you have requested to"
+                " unsubscribe from receiving the newsletter "
+                " from the ${portal_title} website at ${url}. We are glad to "
+                " inform you that you are not subscribed to receive the "
+                " newsletter. This requires no additional action on your "
+                " part.",
                 mapping={
                     "portal_title": portal_title,
                     "url": url,
