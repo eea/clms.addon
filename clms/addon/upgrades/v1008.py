fc4f327bb4b679a19d092a026b11c98edd2ca349
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-07-26 17:09:11 +0200

    change user profile

---
+++ clms/addon/upgrades/v1008.py
@@ -0,0 +1,39 @@
+""" upgrade step implementation """
+# -*- coding: utf-8 -*-
+
+from logging import getLogger
+
+from collective.taxonomy.interfaces import ITaxonomy
+from zope.i18n.interfaces import ITranslationDomain
+from zope.schema.interfaces import IVocabularyFactory
+from zope.component.hooks import getSiteManager
+
+logger = getLogger(__name__)
+
+TAXONOMIES = [
+    "collective.taxonomy.userprofileinstitutionaldomain",
+    "collective.taxonomy.userprofileproductuseintention",
+    "collective.taxonomy.userprofileprofessionalthematicdomain",
+]
+
+
+def delete_taxonomies(context=None):
+    sm = getSiteManager()
+
+    for item in TAXONOMIES:
+        utility = sm.queryUtility(ITaxonomy, name=item)
+        if utility is not None:
+            utility.unregisterBehavior()
+
+            sm.unregisterUtility(utility, ITaxonomy, name=item)
+            sm.unregisterUtility(utility, IVocabularyFactory, name=item)
+            sm.unregisterUtility(utility, ITranslationDomain, name=item)
+
+            logger.info("Unregistered taxonomy: %s", item)
+
+
+def upgrade(setup_tool=None):
+    """upgrade function"""
+    logger.info("Running upgrade (Python): v1008")
+
+    delete_taxonomies(setup_tool)
