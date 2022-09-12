21c917cc4931801eac33ff2beaefcbe3f6f2270b
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-09-12 17:51:21 +0200

    feat: new indexers

--- clms/addon/restapi/block_indexers/indexers.py
+++ clms/addon/restapi/block_indexers/indexers.py
@@ -1,3 +1,4 @@
+""" custom indexers for special blocks"""
 # -*- coding: utf-8 -*-
 from plone.restapi.behaviors import IBlocks
 from plone.restapi.interfaces import IBlockSearchableText
@@ -10,11 +11,14 @@ from plone.restapi.indexers import extract_text
 @implementer(IBlockSearchableText)
 @adapter(IBlocks, IBrowserRequest)
 class TabSearchableText(object):
+    """ Tab content searchable text """
     def __init__(self, context, request):
+        """ constructor """
         self.context = context
         self.request = request
 
     def __call__(self, block_value):
+        """ implementation"""
         blocks = block_value.get('blocks', {})
         blocks_layout = block_value.get('blocks_layout', {})
 
@@ -27,3 +31,56 @@ class TabSearchableText(object):
         # std_text = SearchableText(obj)
         # blocks_text.append(std_text)
         return " ".join([text.strip() for text in blocks_text if text.strip()])
+
+
+@implementer(IBlockSearchableText)
+@adapter(IBlocks, IBrowserRequest)
+class AccordionSearchableText(object):
+    """ Accordion searchable text """
+    def __init__(self, context, request):
+        """ constructor """
+        self.context = context
+        self.request = request
+
+    def __call__(self, block_value):
+        """ implementation"""
+
+        data = block_value.get('data', {})
+
+        blocks = data.get('blocks', {})
+        blocks_layout = data.get('blocks_layout', {})
+
+        blocks_text = []
+        for block_id in blocks_layout.get("items", []):
+            block = blocks.get(block_id, {})
+            blocks_text.append(extract_text(block, self.context, self.request))
+
+        return " ".join([text.strip() for text in blocks_text if text.strip()])
+
+
+@implementer(IBlockSearchableText)
+@adapter(IBlocks, IBrowserRequest)
+class AccordionPanelSearchableText(object):
+    """ accordion panel searchable text"""
+    def __init__(self, context, request):
+        """ constructor """
+        self.context = context
+        self.request = request
+
+    def __call__(self, block_value):
+        """ implementation"""
+
+        blocks = block_value.get('blocks', {})
+        blocks_layout = block_value.get('blocks_layout', {})
+
+        blocks_text = []
+        for block_id in blocks_layout.get("items", []):
+            block = blocks.get(block_id, {})
+            blocks_text.append(extract_text(block, self.context, self.request))
+
+        blocks_text.append(block_value.get('title', ''))
+
+        return " ".join([text.strip() for text in blocks_text if text.strip()])
+
+
+
