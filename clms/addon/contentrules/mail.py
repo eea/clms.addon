622e61f6db7d1e5053a4699a42829b631963b0bb
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-09-29 08:44:34 +0200

    chore: lint

--- clms/addon/contentrules/mail.py
+++ clms/addon/contentrules/mail.py
@@ -26,41 +26,43 @@ from zope.globalrequest import getRequest
 from zope.interface import Interface, implementer
 from zope.interface.interfaces import ComponentLookupError
 
-logger = logging.getLogger('plone.contentrules')
+logger = logging.getLogger("plone.contentrules")
 
 
 class IMailAction(Interface):
-    """Definition of the configuration available for a mail action
-    """
+    """Definition of the configuration available for a mail action"""
+
     subject = schema.TextLine(
-        title=_(u'Subject'),
-        description=_(u'Subject of the message'),
-        required=True
+        title=_(u"Subject"),
+        description=_(u"Subject of the message"),
+        required=True,
     )
     source = schema.TextLine(
-        title=_(u'Email source'),
+        title=_(u"Email source"),
         description=_(
-            'The email address that sends the email. If no email is provided '
-            'here, it will use the portal from address.'
+            "The email address that sends the email. If no email is provided "
+            "here, it will use the portal from address."
         ),
-        required=False
+        required=False,
     )
     recipients = schema.TextLine(
-        title=_(u'Email recipients'),
+        title=_(u"Email recipients"),
         description=_(
-            'The email where you want to send this message. To send it to '
-            'different email addresses, just separate them with ,'
+            "The email where you want to send this message. To send it to "
+            "different email addresses, just separate them with ,"
         ),
-        required=True
+        required=True,
     )
     exclude_actor = schema.Bool(
-        title=_(u'Exclude actor from recipients'),
-        description=_('Do not send the email to the user that did the action.')
+        title=_(u"Exclude actor from recipients"),
+        description=_(
+            "Do not send the email to the user that did the action."
+        ),
     )
     message = schema.Text(
-        title=_(u'Message'),
-        description=_(u'The message that you want to mail.'),
-        required=True
+        title=_(u"Message"),
+        description=_(u"The message that you want to mail."),
+        required=True,
     )
 
 
@@ -70,42 +72,42 @@ class MailAction(SimpleItem):
     The implementation of the action defined before
     """
 
-    subject = u''
-    source = u''
-    recipients = u''
-    message = u''
+    subject = u""
+    source = u""
+    recipients = u""
+    message = u""
     exclude_actor = False
 
-    element = 'plone.actions.Mail'
+    element = "plone.actions.Mail"
 
     @property
     def summary(self):
-        """ description"""
-        return _(u'Email report to ${recipients}',
-                 mapping=dict(recipients=self.recipients))
+        """description"""
+        return _(
+            u"Email report to ${recipients}",
+            mapping=dict(recipients=self.recipients),
+        )
 
 
 @implementer(IExecutable)
 @adapter(Interface, IMailAction, Interface)
 class MailActionExecutor:
-    """The executor for this action.
-    """
+    """The executor for this action."""
 
     def __init__(self, context, element, event):
-        """ initialize the action"""
+        """initialize the action"""
         self.context = context
         self.element = element
         self.event = event
         registry = getUtility(IRegistry)
-        self.mail_settings = registry.forInterface(IMailSchema,
-                                                   prefix='plone')
+        self.mail_settings = registry.forInterface(IMailSchema, prefix="plone")
 
     def __call__(self):
-        """ execute the action """
-        mailhost = getToolByName(aq_inner(self.context), 'MailHost')
+        """execute the action"""
+        mailhost = getToolByName(aq_inner(self.context), "MailHost")
         if not mailhost:
             raise ComponentLookupError(
-                'You must have a Mailhost utility to execute this action'
+                "You must have a Mailhost utility to execute this action"
             )
 
         email_charset = self.mail_settings.email_charset
@@ -126,40 +128,42 @@ class MailActionExecutor:
                 if request:
                     messages = IStatusMessage(request)
                     msg = _(
-                        u'Error sending email from content rule. You must '
-                        u'provide a source address for mail '
-                        u'actions or enter an email in the portal properties'
+                        u"Error sending email from content rule. You must "
+                        u"provide a source address for mail "
+                        u"actions or enter an email in the portal properties"
                     )
-                    messages.add(msg, type=u'error')
+                    messages.add(msg, type=u"error")
                 return False
 
             from_name = self.mail_settings.email_from_name.strip('"')
             if six.PY2 and isinstance(from_name, six.text_type):
-                from_name = from_name.encode('utf8')
+                from_name = from_name.encode("utf8")
             source = '"{0}" <{1}>'.format(from_name, from_address)
 
         recip_string = interpolator(self.element.recipients)
         if recip_string:  # check recipient is not None or empty string
             # pylint: disable=consider-using-set-comprehension
-            recipients = set([
-                str(mail.strip()) for mail in recip_string.split(',')
-                if mail.strip()
-            ])
+            recipients = set(
+                [
+                    str(mail.strip())
+                    for mail in recip_string.split(",")
+                    if mail.strip()
+                ]
+            )
         else:
             recipients = set()
 
         if self.element.exclude_actor:
-            mtool = getToolByName(aq_inner(self.context), 'portal_membership')
+            mtool = getToolByName(aq_inner(self.context), "portal_membership")
             actor_email = mtool.getAuthenticatedMember().getProperty(
-                'email',
-                ''
+                "email", ""
             )
             if actor_email in recipients:
                 recipients.remove(actor_email)
 
         # prepend interpolated message with \n to avoid interpretation
         # of first line as header
-        message = u'\n{0}'.format(interpolator(self.element.message))
+        message = u"\n{0}".format(interpolator(self.element.message))
 
         subject = interpolator(self.element.subject)
 
@@ -176,9 +180,7 @@ class MailActionExecutor:
             msgText = MIMEText("This is the alternative plain text message.")
             msgAlternative.attach(msgText)
 
-            msgHTML = MIMEText(
-                safe_text(message, "utf-8"), "html"
-            )
+            msgHTML = MIMEText(safe_text(message, "utf-8"), "html")
             msgAlternative.attach(msgHTML)
             try:
                 # We're using "immediateTrue" because otherwise we won't
@@ -187,12 +189,17 @@ class MailActionExecutor:
                 # AlecM thinks this wouldn't be a problem if mail queuing was
                 # always on -- but it isn't. (stevem)
                 # so we test if queue is not on to set immediate
-                mailhost.send(msgRoot.as_string(), email_recipient, source,
-                              subject=subject, charset=email_charset,
-                              immediate=not mailhost.smtp_queue)
+                mailhost.send(
+                    msgRoot.as_string(),
+                    email_recipient,
+                    source,
+                    subject=subject,
+                    charset=email_charset,
+                    immediate=not mailhost.smtp_queue,
+                )
             except (MailHostError, SMTPException):
                 logger.exception(
-                    'mail error: Attempt to send mail in content rule failed'
+                    "mail error: Attempt to send mail in content rule failed"
                 )
 
         return True
@@ -202,17 +209,19 @@ class MailAddForm(ActionAddForm):
     """
     An add form for the mail action
     """
+
     schema = IMailAction
-    label = _(u'Add HTML Mail Action')
-    description = _(u'A mail action can mail different recipient.')
-    form_name = _(u'Configure element')
+    label = _(u"Add HTML Mail Action")
+    description = _(u"A mail action can mail different recipient.")
+    form_name = _(u"Configure element")
     Type = MailAction
     # custom template will allow us to add help text
-    template = ViewPageTemplateFile('mail.pt')
+    template = ViewPageTemplateFile("mail.pt")
 
 
 class MailAddFormView(ContentRuleFormWrapper):
-    """ the view"""
+    """the view"""
+
     form = MailAddForm
 
 
@@ -220,15 +229,17 @@ class MailEditForm(ActionEditForm):
     """
     An edit form for the mail action
     """
+
     schema = IMailAction
-    label = _(u'Edit HTML Mail Action')
-    description = _(u'A mail action can mail different recipient.')
-    form_name = _(u'Configure element')
+    label = _(u"Edit HTML Mail Action")
+    description = _(u"A mail action can mail different recipient.")
+    form_name = _(u"Configure element")
 
     # custom template will allow us to add help text
-    template = ViewPageTemplateFile('mail.pt')
+    template = ViewPageTemplateFile("mail.pt")
 
 
 class MailEditFormView(ContentRuleFormWrapper):
-    """ Edit view""""
+    """Edit view"""
+
     form = MailEditForm
