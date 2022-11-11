7e8b8e939801d07ce8f551bd9d6e3190cf582e07
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-11-11 09:04:07 +0100

    feat: do additional checks after EU Login callback

--- clms/addon/browser/loginview.py
+++ clms/addon/browser/loginview.py
@@ -31,43 +31,45 @@ class MyCallBack(BrowserView):
             self.request,
             use_session_data_manager=self.context.use_session_data_manager,
         )
-
-        member = api.user.get_current()
-        login_time = member.getProperty("login_time", "2000/01/01")
-        initial_login_time = member.getProperty(
-            "initial_login_time", "2000/01/01"
-        )
-        if not isinstance(login_time, DateTime):
-            login_time = DateTime(login_time)
-        is_initial_login = login_time == DateTime(
-            "2000/01/01"
-        ) or initial_login_time == DateTime("2000/01/01")
-
-        membership_tool = api.portal.get_tool("portal_membership")
-        membership_tool.loginUser(self.request)
-
         redirect_url = "/"
+        if not api.user.is_anonymous():
+            member = api.user.get_current()
+            if member:
+                login_time = member.getProperty("login_time", "2000/01/01")
+                initial_login_time = member.getProperty(
+                    "initial_login_time", "2000/01/01"
+                )
+                if not isinstance(login_time, DateTime):
+                    login_time = DateTime(login_time)
+                is_initial_login = login_time == DateTime(
+                    "2000/01/01"
+                ) or initial_login_time == DateTime("2000/01/01")
 
-        if is_initial_login:
-            # Allow write-on-read to update user properties on first login
-            alsoProvides(self.request, IDisableCSRFProtection)
-            redirect_url = "/en/profile"
-            member.setMemberProperties(
-                mapping={
-                    "initial_login_time": DateTime(),
-                }
-            )
-        else:
-            came_from = self.request.get("came_from")
-            if not came_from and session:
-                came_from = session.get("came_from")
+                membership_tool = api.portal.get_tool("portal_membership")
+                membership_tool.loginUser(self.request)
 
-            portal_url = api.portal.get_tool("portal_url")
+                redirect_url = "/"
 
-            if came_from:
-                # pylint: disable=line-too-long
-                if came_from.startswith("http") and portal_url.isURLInPortal(came_from) or same_domain(portal_url(), came_from) or not came_from.startswith("http"):  # noqa: E501
-                    redirect_url = came_from
+                if is_initial_login:
+                    # Allow write-on-read to update user properties on first login
+                    alsoProvides(self.request, IDisableCSRFProtection)
+                    redirect_url = "/en/profile"
+                    member.setMemberProperties(
+                        mapping={
+                            "initial_login_time": DateTime(),
+                        }
+                    )
+                else:
+                    came_from = self.request.get("came_from")
+                    if not came_from and session:
+                        came_from = session.get("came_from")
+
+                    portal_url = api.portal.get_tool("portal_url")
+
+                    if came_from:
+                        # pylint: disable=line-too-long
+                        if (came_from.startswith("http") and portal_url.isURLInPortal(came_from) or same_domain(portal_url(), came_from) or not came_from.startswith("http")):  # noqa: E501
+                            redirect_url = came_from
 
         return self.request.response.redirect(redirect_url, status=302)
 
