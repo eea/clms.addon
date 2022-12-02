950dce6f601e99d1a17b0b7ac5e8319542ba72d0
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-12-02 10:01:22 +0100

    lint

--- clms/addon/patches/sortable_title.py
+++ clms/addon/patches/sortable_title.py
@@ -1,9 +1,11 @@
 """ patch the sortable_title max length in Products.CMFPlone"""
 
+from logging import getLogger
+
 from Products.CMFPlone import CatalogTool
 
 CatalogTool.MAX_SORTABLE_TITLE = 99999
 
-from logging import getLogger
+
 log = getLogger(__name__)
 log.info('Patched MAX_SORTABLE_TITLE to allow longer sortable_titles')
