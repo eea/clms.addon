c3ee68880374bd45bd698dbea7231dcad33b2d39
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-02-03 13:14:16 +0100

    fix:add additional controls

--- clms/addon/utils.py
+++ clms/addon/utils.py
@@ -1,3 +1,10 @@
 """ utils for clms.addon """
 # -*- coding: utf-8 -*-
-DIRECT_LINK_PORTAL_TYPES = ['File', 'TechnicalLibrary']
+DIRECT_LINK_PORTAL_TYPES = ["File", "TechnicalLibrary"]
+
+
+CLMS_DOMAINS = [
+    "clmsdemo.devel6cph.eea.europa.eu",
+    "clms-staging.eea.europa.eu",
+    "clms-prod.eea.europa.eu",
+]
