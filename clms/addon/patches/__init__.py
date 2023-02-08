91eaa15b253e0956b6d98a8175f914c9748a993c
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-02-08 16:36:53 +0100

    disable transformes and patches

--- clms/addon/patches/__init__.py
+++ clms/addon/patches/__init__.py
@@ -3,8 +3,8 @@
 
 from logging import getLogger
 import clms.addon.patches.meeting as a
-import clms.addon.patches.restapi as b
-import clms.addon.patches.blocks as c
+# import clms.addon.patches.restapi as b
+# import clms.addon.patches.blocks as c
 import clms.addon.patches.sortable_title as d
 import clms.addon.patches.behavior as e
 
@@ -12,7 +12,7 @@ log = getLogger(__name__)
 
 # Stupid logging to avoid pyflakes errors
 log.info(a)
-log.info(b)
-log.info(c)
+# log.info(b)
+# log.info(c)
 log.info(d)
 log.info(e)
