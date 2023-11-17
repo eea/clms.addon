cd2bf30f80e87addf3b303308777cd0322633bc5
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-17 14:30:20 +0100

    lint

--- clms/addon/patches/__init__.py
+++ clms/addon/patches/__init__.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from logging import getLogger
-import clms.addon.patches.meeting as a
 
-import clms.addon.patches.sortable_title as d
 import clms.addon.patches.behavior as e
-import clms.addon.patches.scale as f
 import clms.addon.patches.contextnavigation as g
+import clms.addon.patches.meeting as a
+import clms.addon.patches.scale as f
+import clms.addon.patches.sortable_title as d
 
 log = getLogger(__name__)
 
