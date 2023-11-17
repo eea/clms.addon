b01dabeb59311337e37531db41714238cf0fb82c
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-17 14:25:18 +0100

    feat: add dummy interface to fake eea.api.taxonomy

--- clms/addon/__init__.py
+++ clms/addon/__init__.py
@@ -3,7 +3,9 @@
 from logging import getLogger
 
 import clms.addon.patches as a
+from plone.app.upgrade.utils import alias_module
 from zope.i18nmessageid.message import MessageFactory
+from zope.interface import Interface
 
 _ = MessageFactory("clms.addon")
 
@@ -11,6 +13,16 @@ _ = MessageFactory("clms.addon")
 log = getLogger(__name__)
 # Stupid logging to avoid linter errors
 log.info(a)
+
+
+class IDummy(Interface):
+    """Dummy interface to alias eea.api.taxonomy.interfaces.IEeaApiTaxonomyLayer"""
+
+
+try:
+    from eea.api.taxonomy.interfaces import IEeaApiTaxonomyLayer  # noqa
+except ImportError:
+    alias_module("eea.api.taxonomy.interfaces.IEeaApiTaxonomyLayer", IDummy)
 
 
 def initialize(context):
