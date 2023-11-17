493295a31d01cfba7b6d760f374659f1a1570a9d
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-17 14:28:25 +0100

    feat: delete unused patches

--- clms/addon/patches/__init__.py
+++ clms/addon/patches/__init__.py
@@ -4,8 +4,6 @@
 from logging import getLogger
 import clms.addon.patches.meeting as a
 
-# import clms.addon.patches.restapi as b
-# import clms.addon.patches.blocks as c
 import clms.addon.patches.sortable_title as d
 import clms.addon.patches.behavior as e
 import clms.addon.patches.scale as f
@@ -15,8 +13,6 @@ log = getLogger(__name__)
 
 # Stupid logging to avoid pyflakes errors
 log.info(a)
-# log.info(b)
-# log.info(c)
 log.info(d)
 log.info(e)
 log.info(f)
