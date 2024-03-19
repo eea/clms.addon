a3596e5af12b571da6a6e11b8272bfe6c028c2db
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-03-19 19:54:48 +0100

    add email substition for content rule configuration

---
+++ clms/addon/contentrules/productionupdates_email_substitution.py
@@ -0,0 +1,24 @@
+"""
+Content rule string interpolator to return subscribed email address
+"""
+
+from plone.stringinterp.adapters import BaseSubstitution
+from zope.component import adapter, getUtility
+from zope.interface import Interface
+
+from clms.addon import _
+from clms.addon.utilities.productionupdates_notifications_utility import (
+    IProductionUpdatesNotificationsUtility
+)
+
+@adapter(Interface)
+class ProductionUpdatesSubscriberSubstitution(BaseSubstitution):
+    """ Subscriber substitution adapter"""
+
+    category = _("Portal")
+    description = _("Email addresses subscribed to production updates notifications")
+
+    def safe_call(self):
+        """ call the utility to get the subscribers """
+        utility = getUtility(IProductionUpdatesNotificationsUtility)
+        return ",".join(list(utility.list_subscribers().keys()))
