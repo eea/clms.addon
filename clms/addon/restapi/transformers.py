14f52a34b4d883cf28606b4b41155a545396e5d7
GhitaB <ghitabizau27@gmail.com>
2024-09-26 11:38:32 +0300

    Refs #277384 - Solve link in a table case.

--- clms/addon/restapi/transformers.py
+++ clms/addon/restapi/transformers.py
@@ -80,6 +80,63 @@ DOWNLOADABLE_TYPES = ["File", "TechnicalLibrary", "Image"]
 FILE_FIELDS = {"File": "file", "TechnicalLibrary": "file", "Image": "image"}
 
 
+def handle_links(child):
+    """Solve a href cases"""
+    external = child.get("data", {}).get("link", {}).get("external", {})
+
+    internal = child.get("data", {}).get("link", {}).get("internal", {})
+
+    if external:
+        # Check if it has an explicit target
+        target = external.get("target", None)
+        if target is None:
+            # It has no explicit target, mark it to be
+            # opened in a new window with target=_blank
+            child["data"]["link"]["external"]["target"] = "_blank"
+
+            # self.log.info(
+            #     "Link converted to target=_blank: %s",
+            #     child["data"]["link"]["external"]["external_link"],
+            # )
+    elif internal:
+        # {
+        #     "children": [
+        #         {"text": "What is the European Ground Motion Service?"}
+        #     ],
+        #     "data": {
+        #         "link": {
+        #             "internal": {
+        #                 "internal_link": [
+        #                     {
+        #                         "@id": "../../../resolveuid/53b80c704a814502bd088e617ac11afc",
+        #                         "title": "Introduction to EGMS",
+        #                     }
+        #                 ]
+        #             }
+        #         }
+        #     },
+        #     "type": "a",
+        # }
+        internal_link = internal.get("internal_link", [])
+        for info in internal_link:
+            oid = info.get("@id", "")
+            if oid and "resolveuid" in oid:
+                uid = oid.split("/resolveuid/")[1]
+                obj = api.content.get(UID=uid)
+                if obj is None:
+                    # self.log.info("Could not find obj for uid %s", uid)
+                    continue
+
+                if obj.portal_type in DOWNLOADABLE_TYPES:
+                    info["download"] = True
+                    info["file_field"] = FILE_FIELDS[obj.portal_type]
+                    # print("setting to download", info)
+
+                # print(obj, obj.portal_type)
+                # if hasattr(obj, "file"):
+                #     info["download"] = True
+
+
 class SlateExternalLinkBlockSerializerBase:
     """Slate block serializer to handle external links that are not
     explicitely marked to be opened in a given target.
@@ -105,62 +162,7 @@ class SlateExternalLinkBlockSerializerBase:
         for child in children:
             node_type = child.get("type")
             if node_type == "a":
-                external = child.get("data", {}).get(
-                    "link", {}).get("external", {})
-
-                internal = child.get("data", {}).get(
-                    "link", {}).get("internal", {})
-
-                if external:
-                    # Check if it has an explicit target
-                    target = external.get("target", None)
-                    if target is None:
-                        # It has no explicit target, mark it to be
-                        # opened in a new window with target=_blank
-                        child["data"]["link"]["external"]["target"] = "_blank"
-
-                        self.log.info(
-                            "Link converted to target=_blank: %s",
-                            child["data"]["link"]["external"]["external_link"],
-                        )
-                elif internal:
-                    # {
-                    #     "children": [
-                    #         {"text": "What is the European Ground Motion Service?"}
-                    #     ],
-                    #     "data": {
-                    #         "link": {
-                    #             "internal": {
-                    #                 "internal_link": [
-                    #                     {
-                    #                         "@id": "../../../resolveuid/53b80c704a814502bd088e617ac11afc",
-                    #                         "title": "Introduction to EGMS",
-                    #                     }
-                    #                 ]
-                    #             }
-                    #         }
-                    #     },
-                    #     "type": "a",
-                    # }
-                    internal_link = internal.get("internal_link", [])
-                    for info in internal_link:
-                        oid = info.get("@id", "")
-                        if oid and "resolveuid" in oid:
-                            uid = oid.split("/resolveuid/")[1]
-                            obj = api.content.get(UID=uid)
-                            if obj is None:
-                                self.log.info(
-                                    "Could not find obj for uid %s", uid)
-                                continue
-
-                            if obj.portal_type in DOWNLOADABLE_TYPES:
-                                info["download"] = True
-                                info["file_field"] = FILE_FIELDS[obj.portal_type]
-                                # print("setting to download", info)
-
-                            # print(obj, obj.portal_type)
-                            # if hasattr(obj, "file"):
-                            #     info["download"] = True
+                handle_links(child)
 
         return block
 
@@ -191,6 +193,8 @@ class SlateTableBlockTransformer(SlateBlockTransformer):
                 for child in children:
                     node_type = child.get("type")
                     if node_type:
+                        if node_type == "a":
+                            handle_links(child)
                         handler = getattr(self, f"handle_{node_type}", None)
                         if handler:
                             handler(child)
