86475af5bd8f84413c3a56b149ca663922dacc6e
GhitaB <ghitabizau27@gmail.com>
2025-01-30 11:09:08 +0200

    Refs #283251 - Override DateTimeFieldSerializer to fix issues for Folder items.

--- clms/addon/interfaces.py
+++ clms/addon/interfaces.py
@@ -2,8 +2,10 @@
 
 # from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.restapi.interfaces import IPloneRestapiLayer
+from eea.volto.policy.interfaces import IEeaVoltoPolicyLayer
 
 
 # inheriting from IPloneRestapiLayer makes sure our services have higher specificity
-class IClmsAddonLayer(IPloneRestapiLayer):
+# same for eea.volto.policy layer
+class IClmsAddonLayer(IPloneRestapiLayer, IEeaVoltoPolicyLayer):
     """Marker interface that defines a browser layer."""
---
+++ clms/addon/restapi/dxfields.py
@@ -0,0 +1,50 @@
+"""DXFields - override solution from eea.volto.policy in order to fix
+our Folder case (of effective date issues)
+"""
+
+from plone.app.dexterity.behaviors.metadata import IPublication
+from plone.dexterity.interfaces import IDexterityContent
+from plone.restapi.interfaces import IFieldSerializer
+from plone.restapi.serializer.dxfields import DefaultFieldSerializer
+from plone.restapi.serializer.dxfields import DefaultPrimaryFieldTarget
+from plone.namedfile.interfaces import INamedFileField
+from zope.component import adapter
+from zope.interface import implementer
+from zope.schema.interfaces import IDatetime
+
+from plone.app.contenttypes.interfaces import IFolder
+
+from clms.addon.interfaces import IClmsAddonLayer
+
+try:
+    from eea.coremetadata.metadata import ICoreMetadata
+except ImportError:
+    # Fallback
+    ICoreMetadata = IPublication
+
+
+# @adapter(IDatetime, IDexterityContent, IClmsAddonLayer)
+@adapter(IDatetime, IFolder, IClmsAddonLayer)
+@implementer(IFieldSerializer)
+class DateTimeFieldSerializer(DefaultFieldSerializer):
+    """DateTimeFieldSerializer"""
+
+    def get_value(self, default=None):
+        """Get value"""
+        value = getattr(
+            self.field.interface(self.context), self.field.__name__, default
+        )
+        if value and self.field.interface in (
+            IPublication,
+            ICoreMetadata,
+        ):
+            # the patch: we want the dates with full tz infos
+            # default value is taken from
+            # plone.app.dexterity.behaviors.metadata.Publication that escape
+            # timezone
+            try:
+                result = getattr(self.context, self.field.__name__)()
+            except Exception:
+                result = getattr(self.context, self.field.__name__)
+            return result
+        return value
