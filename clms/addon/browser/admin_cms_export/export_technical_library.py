4abd907aa4c6e75ebea38aa24b563d5bd55de713
GhitaB <ghitabizau27@gmail.com>
2025-03-06 11:57:37 +0200

    Refs #278140 - CMS Content Export: export technical library.

--- clms/addon/browser/admin_cms_export/export_technical_library.py
+++ clms/addon/browser/admin_cms_export/export_technical_library.py
@@ -7,13 +7,11 @@ from plone.app.uuid.utils import uuidToObject
 import csv
 import io
 
-
-MAX_NUM_OF_RELATIONS = 10
+MAX_NUM_OF_RELATIONS = 25
 
 
 def prepare_item_data(technical_library):
-    """ Prepare technical library item data
-    """
+    """Prepare technical library item data"""
     item = {}
     item["item_technical_library_id"] = technical_library.UID()
     item["item_technical_library_title"] = technical_library.title
@@ -26,34 +24,33 @@ def prepare_item_data(technical_library):
     if products is None:
         products = []
 
-    # TODO WIP here
-    # for i, product_id in enumerate(products):
-    #     item[f"related_product_{i}_id"] = product_id
-    #     product = uuidToObject(product_id)
-    #     product_title = product.title if product is not None else ""
-    #     item[f"related_product_{i}_title"] = product_title
-    #
-    # try:
-    #     datasets = technical_library.datasets
-    # except Exception:
-    #     datasets = []
-    # if datasets is None:
-    #     datasets = []
-    #
-    # for i, dataset_id in enumerate(datasets):
-    #     item[f"related_dataset_{i}_id"] = dataset_id
-    #     dataset = uuidToObject(dataset_id)
-    #     dataset_title = dataset.title if dataset is not None else ""
-    #     item[f"related_dataset_{i}_title"] = dataset_title
-    #
-    # # fill with empty columns
-    # for i in range(MAX_NUM_OF_RELATIONS):
-    #     if f"related_product_{i}_id" not in item:
-    #         item[f"related_product_{i}_id"] = ""
-    #         item[f"related_product_{i}_title"] = ""
-    #     if f"related_dataset_{i}_id" not in item:
-    #         item[f"related_dataset_{i}_id"] = ""
-    #         item[f"related_dataset_{i}_title"] = ""
+    for i, product_id in enumerate(products):
+        item[f"related_product_{i}_id"] = product_id
+        product = uuidToObject(product_id)
+        product_title = product.title if product is not None else ""
+        item[f"related_product_{i}_title"] = product_title
+
+    try:
+        datasets = technical_library.datasets
+    except Exception:
+        datasets = []
+    if datasets is None:
+        datasets = []
+
+    for i, dataset_id in enumerate(datasets):
+        item[f"related_dataset_{i}_id"] = dataset_id
+        dataset = uuidToObject(dataset_id)
+        dataset_title = dataset.title if dataset is not None else ""
+        item[f"related_dataset_{i}_title"] = dataset_title
+
+    # fill with empty columns
+    for i in range(MAX_NUM_OF_RELATIONS):
+        if f"related_product_{i}_id" not in item:
+            item[f"related_product_{i}_id"] = ""
+            item[f"related_product_{i}_title"] = ""
+        if f"related_dataset_{i}_id" not in item:
+            item[f"related_dataset_{i}_id"] = ""
+            item[f"related_dataset_{i}_title"] = ""
 
     return item
 
@@ -76,9 +73,8 @@ def export_technical_library(request):
     )
 
     output = io.StringIO()
-    fieldnames = libraries[0].keys()
-    writer = csv.DictWriter(output, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(libraries)
+
+    csv.DictWriter(output, sorted(libraries[0].keys())).writeheader()
+    csv.DictWriter(output, sorted(libraries[0].keys())).writerows(libraries)
 
     return output.getvalue()
