c3d097d259c3233a856fbba85450230e20177813
ana-oprea <anaop3212@gmail.com>
2025-06-16 11:45:34 +0300

    feat: add BYOC Collection field to the properties returned in the cart -refs #287644

--- clms/addon/restapi/cart_dataset_search/get.py
+++ clms/addon/restapi/cart_dataset_search/get.py
@@ -85,4 +85,5 @@ def get_needed_values(res):
         download_show_auxiliary_calendar=res.get(
             "download_show_auxiliary_calendar", False
         ),
+        byoc_collection_id=res.get("byoc_collection_id", ""),
     )
