6e051e453d8093ee73ceea8e6aa1771c876dd69f
GhitaB <ghitabizau27@gmail.com>
2025-08-15 10:30:43 +0300

    Refs #289902 - WIP monitor CDSE tasks. Extract the list of current CDSE tasks and do nothing for cancelled tasks.

--- clms/addon/browser/cdse/monitor.py
+++ clms/addon/browser/cdse/monitor.py
@@ -6,6 +6,8 @@ import logging
 from Products.Five import BrowserView
 from clms.addon.browser.cdse.config import CDSE_MONITOR_VIEW_TOKEN_ENV_VAR
 from clms.addon.browser.cdse.utils import get_env_var
+from clms.downloadtool.utility import IDownloadToolUtility
+from zope.component import getUtility
 
 logger = logging.getLogger("clms.addon")
 
@@ -22,9 +24,52 @@ class CDSEBatchStatusMonitor(BrowserView):
     """ Check status of CDSE downloads """
 
     def check_cdse_status(self):
-        """ WIP """
+        """ Check the status for CDSE tasks """
+        utility = getUtility(IDownloadToolUtility)
 
-        logger.info("WIP Check CDSE status")
+        logger.info(f"START: Get downloads list...")
+        tasks = utility.datarequest_inspect()
+        logger.info(f"END: Get downloads list...")
+
+        logger.info(f"START: Filter by dataset source...")
+        filtered = [
+            t for t in tasks
+            if any(
+                ds.get("DatasetSource") == "CDSE" for ds in t.get(
+                    "Datasets", []))
+        ]
+        logger.info(f"END: Filter by dataset source...")
+
+        logger.info(f"FOUND {len(filtered)} CDSE download requests.")
+
+        for task in filtered:
+            user = task.get('UserID', 'Unknown user')
+            task_id = task.get('TaskId', 'Unknown TaskId')
+            fme_task_id = task.get('FMETaskId', 'Unknown FMETaskId')
+            status = task.get('Status', 'Unknown Status')
+
+            datasets = task.get('Datasets', [])
+            if datasets:
+                for dataset in datasets:
+                    dataset_title = dataset.get('DatasetTitle', 'Unknown')
+                    dataset_id = dataset.get('DatasetID', 'Unknown DatasetID')
+                    logger.info(
+                        f"Task {task_id} (FMEID {fme_task_id}) for user {user} "
+                        f"has status '{status}'. Dataset: '{dataset_title}' "
+                        f"(ID {dataset_id})."
+                    )
+            else:
+                logger.info(f"Task {task_id} (FME {fme_task_id}) is empty.")
+
+            if status == "Cancelled":
+                logger.info("Nothing to do for this task.")
+            else:
+                logger.info("WIP Check CDSE status, update in download tool.")
+
+                # CDSE status check
+                # Updates in download tool.
+
+        logger.info("DONE.")
 
         return "done"
 
