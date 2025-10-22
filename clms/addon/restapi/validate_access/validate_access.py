9c27a43c282558ed08910b2e5738a30084503a71
GhitaB <ghitabizau27@gmail.com>
2025-10-22 11:56:36 +0300

    Refs #293425 - Investigate saved tokens, try to filter by user id.

--- clms/addon/restapi/validate_access/validate_access.py
+++ clms/addon/restapi/validate_access/validate_access.py
@@ -2,7 +2,7 @@ import logging
 from plone.restapi.services import Service
 from plone import api
 from AccessControl.users import SpecialUser
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 logger = logging.getLogger(__name__)
 
@@ -49,30 +49,40 @@ class ValidateTokenAccess(Service):
 
         logger.info(f"Token valid for {name}")
 
-        auth_header = self.request.getHeader("Authorization")
-        auth = ""
-        try:
-            if auth_header and auth_header.startswith("Bearer "):
-                auth = auth_header.split(" ", 1)[1].strip()
-        except Exception:
-            logger.info("Cannot get Bearer value.")
-
         tokens = self.context.acl_users.token_auth._credential_storage[
             'access_tokens']
-        token_data = tokens.get(auth)
+
+        user_id = name
+
+        valid_tokens = []
+        for k, t in tokens.items():
+            logger.info(k)
+            logger.info(t)
+            if t.get("user_id") == user_id:
+                issued = t["issued"]
+                expires_in = t["expires_in"]
+                expires_at = issued + timedelta(seconds=expires_in)
+                if expires_at > datetime.now():
+                    valid_tokens.append(t)
+
+        token_data = None
+        if valid_tokens:
+            token_data = max(valid_tokens, key=lambda x: x["issued"])
+
         # {'key_id': '68a770021b510e078e191235f6asdasdasd',
         # 'user_id': 'userhere',
         # 'issued': datetime.datetime(2025, 10, 21, 15, 57, 8, 19218),
         # 'expires_in': 3600
         # }
         expires_str = ""
-        try:
-            issued = token_data.get("issued")
-            expires_in = token_data.get("expires_in")
-            expires_at = issued + timedelta(seconds=expires_in)
-            expires_str = expires_at.isoformat()  # "2025-10-09T12:00:00Z"
-        except Exception:
-            logger.info("Error expires in.")
+        if token_data is not None:
+            try:
+                issued = token_data.get("issued")
+                expires_in = token_data.get("expires_in")
+                expires_at = issued + timedelta(seconds=expires_in)
+                expires_str = expires_at.isoformat()  # "2025-10-09T12:00:00Z"
+            except Exception:
+                logger.info("Error expires in.")
 
         return self.rsp(
             expires=expires_str, code=200, valid=True, msg="Token is valid")
